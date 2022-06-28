3139dd2dc3349803cbb4746817f67f179fc4c733
FNU Shariful <robinmbstu12@gmail.com>
2022-06-27 23:04:26 -0400

    devskill contested.

--- main.cpp
+++ main.cpp
@@ -1,66 +1,17 @@
 #include "iostream"
 #include "vector"
-#include "Two_Sum.h"
-#include "Monk and Rotation.h"
-#include "MonkandInversions.h"
-#include "JamesandtheMenus.h"
-#include "CF_GR_21_A.h"
-#include "_121BestTimetoBuyandSellStock.h"
+#include "string"
+#include "sstream"
+
 
 using namespace std;
-unsigned binarySearch(vector<int> & nums, int key)
-{
-    unsigned lw = 0, hi = nums.size()-1, mid;
-
-
-    while( lw < hi )
-    {
-        mid = (hi + lw )/2;
-//        cout<<mid<<" "<<key<<endl;
-        if( nums[mid] == key ) return mid;
-        else if( nums[mid] < key ) hi = mid-1;
-        else lw = mid + 1;
-    }
-    return -1;
-}
-
-vector<int> twoSum(vector<int>& nums, int target) {
-
-    for( unsigned i = 0; i< nums.size(); i++ )
-    {
-        unsigned key = target - nums[i];
-//        cout<<key<<endl;
-        unsigned keyPosition = binarySearch(nums,key);
-//        cout<<keyPosition<<endl;
-        if( keyPosition != -1 )
-//            cout<<"keyPosition "<<keyPosition<<endl;
-            break;
-        cout<<key<<endl;
-    }
-
-}
 
 int main()
 {
-//    vector<int> nums;
-//    int value;
-//    for( int i = 0; i<4; i++ )
-//    {
-//        cin>>value;
-//        nums.emplace_back(value);
-//    }
-//    unsigned target;
-//    cin>>target;
-//    twoSum(nums,target);
-//    function1();
-      int n,val;
-      cin>>n;
-      vector<int> p;
-      for( int i=0; i<n; i++ ) {
-          cin>>val;
-          p.emplace_back(val);
-      }
-      _121BestTimetoBuyandSellStock ob1;
-      ob1.maxProfit(p);
+    long long n,m;
+    while (cin>>n>>m)
+    {
+        cout<<n*m/2<<endl;
+    }
     return 0;
 }
